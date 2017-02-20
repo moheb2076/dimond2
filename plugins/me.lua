6864523c9e2291dcc521617760103ef869ef424f
moheb2076 <moheb2076@yahoo.com>
2017-02-20 14:05:37 +0330

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -3,11 +3,11 @@ do
 local function run(msg, matches)
 if matches[1]=="me" and is_sudo(msg) then 
 return  "شما صاحب ربات و سودو میباشید"
-elseif matches[1]=="مقام من" and is_admin(msg) then 
-return  "شما ادمین ربات و یک مقام پایین تر ازسودو میباشید"
-elseif matches[1]=="مقام من" and is_owner(msg) then 
+elseif matches[1]=="me" and is_admin(msg) then 
+return  "شما ادمین ربات میباشید"
+elseif matches[1]=="me" and is_owner(msg) then 
 return  "شما سازنده و صاحب گروه میباشید"
-elseif matches[1]=="مقام من" and is_mod(msg) then 
+elseif matches[1]=="me" and is_mod(msg) then 
 return  "شما کمک مدیراین گروه میباشید"
 else
 return  "شما هیچ مقامی در ربات ندارید"
