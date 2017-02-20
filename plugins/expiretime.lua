380c58a0187df58e81937fe69c94ca9ce41e1983
moheb2076 <moheb2076@yahoo.com>
2017-02-20 13:33:09 +0330

    Update expiretime.lua

--- plugins/expiretime.lua
+++ plugins/expiretime.lua
@@ -14,12 +14,12 @@ local function pre_process(msg)
 	end
 	if tonumber(timetoexpire) == 0 then
 		if redis:hget('expires0',msg.chat_id_) then end
-		tdcli.sendMessage(msg.chat_id_, 0, 1, '*⚠️تمدید ربات تمام شد از ایدی روبه رو برای شارژ کردن استفاده نمایید@you4u*.', 1, 'md')
+		tdcli.sendMessage(msg.chat_id_, 0, 1, '*⚠️تمدید ربات تمام شد از ایدی روبه رو برای شارژ کردن استفاده نمایید@abd2076 @Antispamseed_bot*.', 1, 'md')
 		redis:hset('expires0',msg.chat_id_,'5')
 	end
 	if tonumber(timetoexpire) == 1 then
 		if redis:hget('expires1',msg.chat_id_) then end
-		tdcli.sendMessage(msg.chat_id_, 0, 1, '*⚠️ ⚠️ فقط 1⃣ روز  تا پایان مدت استفاده از ربات باقی مانده است لطفا آن را تمدید کنید.*.', 1, 'md')
+		tdcli.sendMessage(msg.chat_id_, 0, 1, '*⚠️ ⚠️ فقط 1⃣ روز  تا پایان مدت استفاده از ربات باقی مانده است لطفا آن را تمدید کنید. جهت تمدید به @abd2076 @Antispamseed_bot مراجعه شود*.', 1, 'md')
 		redis:hset('expires1',msg.chat_id_,'5')
 	end
 
